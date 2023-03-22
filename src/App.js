edf6d988613b12b0f7e24e0bef89c12bd1b300bd
suraj <mouryasuraj6266444@gmail.com>
2023-03-22 16:53:33 +0530

    New Project Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [data, setData] = useState([]);
+
+  const addData=()=>{
+    setData({
+      name, email, mobile
+  });
+  };
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="textField">
+        <Stack spacing={2} direction="column">
+          <div>
+            <Stack spacing={2} direction="row">
+              <TextField id="outlined-basic" value={name} onChange={(e)=> setName(e.target.value)} label="Name" variant="outlined" />
+              <TextField id="outlined-basic" value={email} onChange={(e)=> setEmail(e.target.value)}  label="Email" variant="outlined" />
+            </Stack>
+          </div>
+          <TextField
+            id="outlined-number"
+            label="Mobile Number"
+            type="number"
+            value={mobile} 
+            onChange={(e)=> setMobile(e.target.value)}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <Button onClick={addData} className="addBtn" color="success" variant="contained">
+            Add
+          </Button>
+        </Stack>
+      </div>
     </div>
-  );
-}
+  )
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+
+
+const Header = () => {
+  return (
+    <div className="header">Record Keeping App</div>
+  )
+}
+
+export default Header
