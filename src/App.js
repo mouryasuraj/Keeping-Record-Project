7faf1aa2d7bce7e51e05e26f1f47a7a603c28080
suraj <mouryasuraj6266444@gmail.com>
2023-03-25 11:51:37 +0530

    Updated the record keeping project

--- src/App.js
+++ src/App.js
@@ -3,49 +3,111 @@ import Header from "./components/Header";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 import { useState } from "react";
 
 function App() {
+  const [form, setForm] = useState({});
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [mobile, setMobile] = useState("");
   const [data, setData] = useState([]);
 
-  const addData=()=>{
-    setData({
-      name, email, mobile
-  });
+  const addData = () => {
+    if (form.name === "" || form.email === "" || form.mobile === "") {
+      alert("Please fill all the fields");
+    } else {
+      setData([...data, form]);
+      setForm(form);
+    }
   };
-  console.log(data)
+
+  const removeItem = (index) => {
+    const arr = data;
+    arr.splice(index, 1);
+    setData([...arr]);
+  };
+
   return (
     <div className="App">
+      {/* /* Header */}
       <Header />
+      {/* Form Started */}
       <div className="textField">
         <Stack spacing={2} direction="column">
           <div>
             <Stack spacing={2} direction="row">
-              <TextField id="outlined-basic" value={name} onChange={(e)=> setName(e.target.value)} label="Name" variant="outlined" />
-              <TextField id="outlined-basic" value={email} onChange={(e)=> setEmail(e.target.value)}  label="Email" variant="outlined" />
+              <TextField
+                id="outlined-basic"
+                value={form.name}
+                onChange={(e) => setForm({...form, name: e.target.value})}
+                label="Name"
+                variant="outlined"
+              />
+              <TextField
+                id="outlined-basic"
+                value={form.email}
+                onChange={(e) => setForm({...form, email: e.target.value})}
+                label="Email"
+                variant="outlined"
+              />
             </Stack>
           </div>
           <TextField
             id="outlined-number"
             label="Mobile Number"
             type="number"
-            value={mobile} 
-            onChange={(e)=> setMobile(e.target.value)}
+            value={form.mobile}
+            onChange={(e) => setForm({...form, mobile: e.target.value})}
             InputLabelProps={{
               shrink: true,
             }}
           />
-          <Button onClick={addData} className="addBtn" color="success" variant="contained">
+          <Button
+            onClick={addData}
+            className="addBtn"
+            color="success"
+            variant="contained"
+          >
             Add
           </Button>
         </Stack>
       </div>
+      {/* Form Ended */}
+
+      {/* Display Show Data Started */}
+      <div className="DataDisplay">
+        <div className="HeadingName">
+          <div className="head">
+            <h4>Name</h4>
+          </div>
+          <div className="head">
+            <h4>Email</h4>
+          </div>
+          <div className="head">
+            <h4>Mobile</h4>
+          </div>
+          <div className="head">
+            <h4>Remove</h4>
+          </div>
+        </div>
+        {data.map((e, index) => {
+          return (
+            <div className="dataDisplay">
+              <div className="head">{e.name}</div>
+              <div className="head">{e.email}</div>
+              <div className="head">{e.mobile}</div>
+              <div className="head">
+                <RemoveCircleIcon
+                  onClick={() => removeItem(index)}
+                  className="removeIcon"
+                />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      {/* Display Show Data Ended */}
     </div>
-  )
-};
+  );
+}
 
 export default App;
